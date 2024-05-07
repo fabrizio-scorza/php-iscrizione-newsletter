6774caa7549d8c94fb718b9768a46703a851a49b
fabrizio-scorza <fabrizio.a.scorza@gmail.com>
2024-05-07 14:51:43 +0200

    FIRST COMMIT html bootstrap

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Newsletter</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body class="bg-dark">
+    <div class="container">
+        <div class="text-white">
+            <h1>Registrazione alla Newsletter</h1>
+            <h3>inserisic i dati e invia per registrarti alla nostra newsletter</h3>
+        </div>
+        <form action="" method="POST" class="form-control">
+            <label for="email">Inserisci la tua e-mail: </label>
+            <input type="email" name="email" id="email" placeholder="esempio@esempio.es">
+            <input type="submit">
+        </form>
+    </div>
+</body>
+
+</html>

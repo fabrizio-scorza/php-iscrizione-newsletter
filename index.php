f65ce9d2625021e609409fa4c04b7d451d5a76b5
fabrizio-scorza <fabrizio.a.scorza@gmail.com>
2024-05-07 15:09:07 +0200

    FEAT one page php

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php
+$email = $_POST['email'] ?? null;
+if ($email == !null) {
+    if (str_contains($email, '@') && str_contains($email, '.')) {
+?>
+        <div class="alert alert-success" role="alert">
+            la tua e-mail è stata registrata con successo!!
+        </div>
+    <?php
+    } else {
+    ?>
+        <div class="alert alert-warning" role="alert">
+            qualcosa è andato storto, controlla la mail inserita e invia nuovamente!!
+        </div>
+<?php
+    }
+}
 ?>
 
 <!DOCTYPE html>

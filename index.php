31b6c1427ad7d64f525002e5361537fb885961e1
fabrizio-scorza <fabrizio.a.scorza@gmail.com>
2024-05-07 15:24:16 +0200

    fix some bootstrap class

--- index.php
+++ index.php
@@ -29,14 +29,14 @@ if ($email == !null) {
 
 <body class="bg-dark">
     <div class="container">
-        <div class="text-white">
+        <div class="text-white my-5">
             <h1>Registrazione alla Newsletter</h1>
             <h3>inserisic i dati e invia per registrarti alla nostra newsletter</h3>
         </div>
-        <form action="" method="POST" class="form-control">
+        <form action="" method="POST" class="form-control d-flex align-items-center gap-2">
             <label for="email">Inserisci la tua e-mail: </label>
-            <input type="email" name="email" id="email" placeholder="esempio@esempio.es">
-            <input type="submit">
+            <input type="email" name="email" id="email" placeholder="esempio@esempio.es" class="px-3 flex-grow-1">
+            <input type="submit" class="btn btn-dark">
         </form>
     </div>
 </body>
